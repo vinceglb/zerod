d8ef09b801e5c5221c06c63ecdda9a896562ab47
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-07 19:26:01 +0100

    🚨 Ajout d'ESLint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { GetServerSideProps, NextPage } from "next";
 
-export default function Home() {
+interface Props {
+  launch: {
+    mission: string;
+    site: string;
+    timestamp: number;
+    rocket: string;
+  };
+}
+
+const IndexPage: NextPage<Props> = ({ launch }) => {
+  const date = new Date(launch.timestamp);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,8 +27,14 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <h1>Next SpaceX Launch: {launch.mission}</h1>
+        <p>
+          {launch.rocket} will take off from {launch.site} on{" "}
+          {date.toDateString()}
         </p>
 
         <div className={styles.grid}>
@@ -56,10 +74,27 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const response = await fetch("https://api.spacexdata.com/v3/launches/next");
+  const nextLaunch = await response.json();
+  return {
+    props: {
+      launch: {
+        mission: nextLaunch.mission_name,
+        site: nextLaunch.launch_site.site_name_long,
+        timestamp: nextLaunch.launch_date_unix * 1000,
+        rocket: nextLaunch.rocket.rocket_name,
+      },
+    },
+  };
+};
