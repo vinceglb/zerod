35bfe9990df0d56ecb1de3eec1e7261b1bc3c33c
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-07 23:38:26 +0100

    🚨 Correction des derniers lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function App({ Component, pageProps }: AppProps) {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
