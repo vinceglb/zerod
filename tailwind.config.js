a32b034dbb0b313a3d447f90109ec0f708ff1b26
Hugo <cclementehugo@gmail.com>
2020-12-11 15:54:29 +0100

    💄 Config des couleurs pour tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       colors: {
         primary: '#FD4A1E',
         secondary: '#FFE0D8',
+        'lighter-secondary': '#FFFAF6',
+        'dark-grey': '#2C2B26',
+        'light-grey': '#666666',
+        'reddish-brown': '#351E1D',
+        'green': '#1DBF78'
       },
     },
   },
