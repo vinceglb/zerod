af7a907625c0b6bcbd7d7ce11f1b6ec10c2b364c
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-11 21:46:20 +0100

    📝 Ajout des params aux stories

--- components/card/CardMarket.stories.tsx
+++ components/card/CardMarket.stories.tsx
@@ -12,5 +12,8 @@ const Template: Story<ComponentProps<typeof CardMarket>> = (args) => <CardMarket
 
 export const Default = Template.bind({})
 Default.args = {
-  title: 'salut',
+  title: 'Épiceries',
+  desc: 'All food used with full invoice and clear origin.',
+  photoUrl:
+    'https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
 }
--- components/card/CardPref.stories.tsx
+++ components/card/CardPref.stories.tsx
@@ -11,4 +11,8 @@ export default {
 const Template: Story<ComponentProps<typeof CardPref>> = (args) => <CardPref {...args} />
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  title: 'Équilibré',
+  photoUrl:
+    'https://images.unsplash.com/photo-1600335895229-6e75511892c8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+}
