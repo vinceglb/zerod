5ed8d81a9ed2ca07c0e8bc8cef79c1d3397f67ba
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-11 21:10:00 +0100

    Ajout des props à CardPref

--- components/card/CardPref.tsx
+++ components/card/CardPref.tsx
@@ -1,13 +1,19 @@
-export interface CardPrefProps {}
+export interface CardPrefProps {
+  title: string
+  photoUrl: string
+  className?: string | undefined
+}
 
-export const CardPref: React.FC<CardPrefProps> = () => (
-  <div className="w-96 pt-4 flex flex-col items-center">
-    <img
-      className="rounded-lg shadow-lg h-52 w-52 object-cover"
-      src="https://images.unsplash.com/photo-1600335895229-6e75511892c8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-      alt="test"
-    />
-
-    <h2 className="font-bold text-gray-800 text-2xl mt-4">Équiliré</h2>
+/**
+ * Carte préférence
+ */
+export const CardPref: React.FC<CardPrefProps> = ({
+  title,
+  photoUrl,
+  className: customClasses,
+}) => (
+  <div className={`pt-4 w-52 flex flex-col items-center ${customClasses}`}>
+    <img className="rounded-lg shadow-lg h-52 w-52 object-cover" src={photoUrl} alt={title} />
+    <h2 className="font-bold text-gray-800 text-2xl mt-4">{title}</h2>
   </div>
 )
