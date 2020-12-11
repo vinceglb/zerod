c998f0f4790bce0801e600c90cde7694baa3bf99
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-11 21:11:07 +0100

    ⚗ Trucs pas super intéressants

--- pages/page-card.tsx
+++ pages/page-card.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from 'next'
 import { Card } from '../components/card/Card'
 import { CardEta } from '../components/card/CardEta'
+import { SectionPref } from '../components/section/SectionPref'
 
 const PageCard: NextPage = () => {
   return (
     <div className="bg-blue-200 p-10">
       <Card />
       <CardEta />
+      <SectionPref />
     </div>
   )
 }
