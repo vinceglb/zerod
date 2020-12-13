aa353804e812237bbc5156393a024ee8035b3ba8
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-13 15:03:29 +0100

    ✨ Ajout de SectionEta

---
+++ components/section/SectionEta.tsx
@@ -0,0 +1,62 @@
+import { CardEta, CardEtaProps } from '../card/CardEta'
+
+export interface SectionEtaProps {
+  className?: string | undefined
+}
+
+export const SectionEta: React.FC<SectionEtaProps> = (props) => {
+  const etas: CardEtaProps[] = [
+    {
+      title: 'Le Paradis Gourmet',
+      photoUrl: '/images/12525466_512843012232908_8715347469593450563_o-759x500.jpg',
+      spe: 'Spécialités régionales • Vins • Épices',
+      price: '€€',
+    },
+    {
+      title: 'La Dulceria by la Manufacture',
+      photoUrl: '/images/00e1024e-60aa-43dd-aadd-669fc556bedb.jpeg',
+      spe: 'Desserts • Street Food',
+      price: '€€€',
+    },
+    {
+      title: 'El almacén',
+      photoUrl: '/images/67402165_636590410162804_7588699881905061888_o.jpg',
+      spe: 'Empanadas • Café Bio • Alfajores',
+      price: '€',
+    },
+    {
+      title: 'Fratelli Cifarelli',
+      photoUrl: '/images/fratelli-cifarelli-epicerie-italienne-toulouscope-14-768x512.jpg',
+      spe: 'Pâtes • Huiles • Charcuterie • Fromage frais',
+      price: '€€',
+    },
+    {
+      title: 'Ultramarinos Ana',
+      photoUrl: '/images/Ultra_fuera_detalles-4.jpg',
+      spe: 'Fruits et légumes bio • Charcuterie ibérique',
+      price: '€€€',
+    },
+    {
+      title: 'La tienda',
+      photoUrl: '/images/12549058_1662404460681262_511336361054674306_n.jpg',
+      spe: 'Jambons • Fromages • Charcuteries • Tapas',
+      price: '€',
+    },
+  ]
+
+  return (
+    <section className={props.className}>
+      {/* Partie header */}
+      <div className="ml-6">
+        <h1 className="font-bold text-reddish-brown text-2xl">Les pépites du coin</h1>
+        <p className="text-light-grey">Mais chuuut ne le dites à personne</p>
+      </div>
+
+      <div className="mx-6 mt-4 space-y-4">
+        {etas.map((eta) => (
+          <CardEta key={eta.title} {...eta} />
+        ))}
+      </div>
+    </section>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { BobPage } from '../utils/layout-types-next'
 import { IndexHeader } from '../components/index/IndexHeader'
 import { SectionMarket } from '../components/section/SectionMarket'
 import { SectionPref } from '../components/section/SectionPref'
+import { SectionEta } from '../components/section/SectionEta'
 
 interface IndexProps {}
 
@@ -11,6 +12,7 @@ const IndexPage: BobPage<IndexProps> = () => (
     <div className="my-8 lg:w-lg mx-auto">
       <SectionMarket />
       <SectionPref className="mt-16" />
+      <SectionEta className="mt-16" />
     </div>
   </div>
 )
