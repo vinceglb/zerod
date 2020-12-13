efb1ded9d7222b5747c18d596198ce95b2d793e7
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-13 15:01:31 +0100

    💄 Amélioration de la carte eta

--- components/card/CardEta.tsx
+++ components/card/CardEta.tsx
@@ -1,20 +1,25 @@
-export interface CardEtaProps {}
+import Image from 'next/image'
 
-export const CardEta: React.FC<CardEtaProps> = () => (
-  <div className="w-96 pt-4">
-    <img
-      className="rounded-lg shadow-lg h-52 w-full object-cover"
-      src="https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-      alt="test"
-    />
+export interface CardEtaProps {
+  title: string
+  photoUrl: string
+  spe: string
+  price: string
+}
 
-    <div className="mt-6 mb-3">
-      <h2 className="font-bold text-2xl text-gray-800 mb-2">Le Paradis Gourmet</h2>
-      <p className="font-medium">Horaires : 12h-14h / 18h-20h</p>
-      <div className="bg-gray-400 h-px my-2" />
-      <div className="flex justify-between font-medium">
-        <p>Spécialités régionales • Vins • Épices</p>
-        <p>€€</p>
+export const CardEta: React.FC<CardEtaProps> = ({ title, photoUrl, spe, price }) => (
+  <div className="w-full pt-4">
+    <div className="h-52 shadow-lg rounded-lg relative overflow-hidden">
+      <Image src={photoUrl} alt={title} layout="fill" objectFit="cover" quality="50" />
+    </div>
+
+    <div className="mt-4 mb-3">
+      <h2 className="font-bold text-xl text-gray-800 mb-1">{title}</h2>
+      <p>Horaires : 12h-14h / 18h-20h</p>
+      <div className="bg-gray-400 h-px my-1" />
+      <div className="flex justify-between">
+        <p>{spe}</p>
+        <p className="flex-none">{price}</p>
       </div>
     </div>
   </div>
