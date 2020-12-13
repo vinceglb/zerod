49b92ae9324abc63ed2125ee2e5e8abe3469bd2f
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-13 19:37:00 +0100

    ♻️ Refactor vitef

--- components/card/CardEtaProduit.tsx
+++ components/card/CardEtaProduit.tsx
@@ -13,7 +13,11 @@ export const CardEtaProduit: React.FC<CardEtaProduitProps> = (props) => (
     <div className="h-36 rounded-xl relative overflow-hidden my-5">
       <Image src={props.photoUrl} alt={props.title} layout="fill" objectFit="cover" quality="50" />
     </div>
-    <button className="h-10 border-2 w-full border-primary rounded-xl font-bold flex items-center justify-center text-primary group-hover:bg-primary group-hover:text-white transition-colors focus:outline-none">
+    <button
+      className="h-10 border-2 w-full border-primary rounded-xl font-bold flex items-center
+                 justify-center text-primary group-hover:bg-primary group-hover:text-white
+                 transition-colors focus:outline-none"
+    >
       Acheter
     </button>
   </div>
