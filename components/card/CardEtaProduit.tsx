328d6f609a5fe2cb1fd6f56612b8983c6658f7f6
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-13 19:17:56 +0100

    💄 Ajout d'un bouton pour acheter un produit

--- components/card/CardEtaProduit.tsx
+++ components/card/CardEtaProduit.tsx
@@ -10,8 +10,11 @@ export const CardEtaProduit: React.FC<CardEtaProduitProps> = (props) => (
   <div className="bg-white rounded-2xl shadow-lg p-4">
     <h3 className="font-bold text-xl">{props.title}</h3>
     <p className="font-bold text-light-grey">{props.price}</p>
-    <div className="h-36 rounded-xl relative overflow-hidden mt-5">
+    <div className="h-36 rounded-xl relative overflow-hidden my-5">
       <Image src={props.photoUrl} alt={props.title} layout="fill" objectFit="cover" quality="50" />
     </div>
+    <button className="h-10 border-2 w-full border-primary rounded-xl font-bold flex items-center justify-center text-primary hover:bg-primary hover:text-white transition-colors focus:outline-none">
+      Acheter
+    </button>
   </div>
 )
